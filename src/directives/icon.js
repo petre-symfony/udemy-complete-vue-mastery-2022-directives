6206fa00de51f6a1740090c96f5870298afbd8bb
petrero <petrero@gmail.com>
2022-05-18 12:45:36 +0300

    15.212(2) Passing Values to Directives
      There are alternative methods of making the directive dynamic. We can use what vue calls arguments. Arguments are the values you pass in after the calling in the directive. Use an argument to allow us to override the icon completely. In some cases we may end up using different icon set from another library. The icon set may use different class names than font-awesome. We want a way to override the class names the directive generates entirely.
      In the Home component file we add  a column after the v-icon directive. The value after this will be interpreted as the argument. We'll going to update the directive to allow us to completely override the classes instead of modifying a portion of it. We call the argument full. If the full argument is present in the directive will allow the developer to override the class for the icon completely. If the argument is present we reasign the iconClass variable to the binding.value property completely.
      There is a limitation to this. We are limited to one argument at any given moment. You can check for different types of arguments but only support one at a time. For our example one is enough.
      If you refresh the home page you notice the headphone icon is missing. This disappearance is because we are passing in a class that doesn't exist in font-awesome library . Inspecting the element will show that the icon element is present in the document. The class we're using is invalid - headphones-alt. If we want to generate an icon we need to pass in the full class name to the directive. We're testing to see if we can override the whole class with the directive, which we can.

--- src/directives/icon.js
+++ src/directives/icon.js
@@ -1,6 +1,11 @@
 export default {
 	beforeMount(el, binding) {
-		const iconClass = `fa fa-${binding.value} float-right text-green-400 text-2xl`;
+		let iconClass = `fa fa-${binding.value} float-right text-green-400 text-2xl`;
+
+		if (binding.arg === 'full') {
+			iconClass = binding.value;
+		}
+
 		el.innerHTML += `<i class="${iconClass}"></i>`;
 	}
 }
